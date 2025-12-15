de7db37a67d2f32d23f0b3ed367d77bf4251b041
illusion <imondi03@gmail.com>
2025-12-16 01:07:07 +0700

    Добавил новый класс

---
+++ ZombieClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS2ZombiePlague
+{
+    abstract public class ZombieClass
+    {
+        public abstract string DisplayName { get; set; }
+        public abstract string IternalName { get; set; }
+        public abstract int Health {  get; set; }
+        public abstract float Speed { get; set; }
+        public abstract float  Gravity { get; set; }
+    }
+}
